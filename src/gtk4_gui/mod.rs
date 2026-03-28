5f81edb0f21d5b9736faddffea8182242b09ac61
hammerklavier <q5vsx3@163.com>
2026-03-29 03:26:16 +0800

    Add path exclusion area callback to update hash results
    When paths are updated in the exclusion area, the hash result area now
    clears and repopulates with files found after applying exclusions.

--- src/gtk4_gui/mod.rs
+++ src/gtk4_gui/mod.rs
@@ -141,9 +141,6 @@ fn build_ui(app: &Application) {
 
     // 4. Area for displaying all files (found or expected to be found) and hashes.
     let hash_result_area = HashResultArea::new();
-    // for _ in 0..30 {
-    //     hash_result_area.add_result(PathBuf::from("/Path/To/File"), String::from("hash_val"));
-    // }
     content_box.append(&hash_result_area);
 
     // Connect paths_updated callback to execute after file selection completes
@@ -156,6 +153,32 @@ fn build_ui(app: &Application) {
         hash_result_area,
         move || {
             log::debug!("Executing path_add_area path finding process after paths updated...");
+            if let Some(hash_result_area) = hash_result_area {
+                // first clear the result area before adding new results.
+                hash_result_area.remove_all();
+
+                let selected_paths = path_add_area.paths();
+                let excluded_paths = path_exclusion_area.paths();
+                let paths_to_hash =
+                    crate::finder::find_files(&selected_paths, &excluded_paths).unwrap(); // TODO: pop up a warning window.
+                for path in paths_to_hash {
+                    hash_result_area.add_result(path, String::from(""));
+                }
+            }
+        }
+    ));
+    // also for path_exclusion_area
+    path_exclusion_area.connect_paths_updated(glib::clone!(
+        #[weak]
+        path_add_area,
+        #[weak]
+        path_exclusion_area,
+        #[weak_allow_none]
+        hash_result_area,
+        move || {
+            log::debug!(
+                "Executing path_exclusion_area path finding process after paths updated..."
+            );
             if let Some(hash_result_area) = hash_result_area {
                 // first clear the result area before adding new results.
                 hash_result_area.remove_all();
